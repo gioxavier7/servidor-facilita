dc089a6350b392fd6fafc74b3a89209b2b36fff6
Giovanna Soares Xavier <devgioxavier@gmail.com>
2025-11-21 19:20:31 -0300

    ajustando prisma.js

--- prisma.js
+++ prisma.js
@@ -5,9 +5,11 @@ const globalForPrisma = global;
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error", "warn"]
+    log: ["error", "warn"],
+    datasourceUrl: process.env.DATABASE_URL, // garante que sempre usa a URL correta
   });
 
+// Em desenvolvimento, mantém apenas uma instância
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
